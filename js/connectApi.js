8fd772d9c2ba647e7f9479eb3e86e538d8239911
Jonas sousa <jonassousagtr@gmail.com>
2023-02-24 10:23:53 -0300

    Build: Criando uma requisição POST para posibilitar adicionar os videos direto na pag alura play.

--- js/connectApi.js
+++ js/connectApi.js
@@ -5,6 +5,23 @@ async function videoList () {
     return turnedIntoObject;
 };
 
+async function createVideo(titulo, descricao, url, imagem) {
+    const conect = await fetch("http://localhost:3000/videos", {
+        method:"POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const convertedApi = await conect.json();
+    return convertedApi
+};
 
 export const connectApi = {
     videoList
