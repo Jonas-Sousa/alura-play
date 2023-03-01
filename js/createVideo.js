f43bb5899beec4a7ef47e4b8aeeedf972e663196
Jonas sousa <jonassousagtr@gmail.com>
2023-03-01 16:49:27 -0300

    Build: Criando arquivo js createApi, para possibilitar o envio dinâmico para a pag Alura play, e atualizar em tempo real o arquivo db.json.

---
+++ js/createVideo.js
@@ -0,0 +1,20 @@
+import { connectApi } from "./connectApi.js";
+
+const form = document.querySelector("[data-form]");
+
+async function creatingVideo(evento) {
+    evento.preventDefault();
+
+    const imagem = document.querySelector("[data-image]").value;
+    const url = document.querySelector("[data-url]").value;
+    const titulo = document.querySelector("[data-title]").value;
+    const descricao = Math.floor(Math.random() * 10).toString()
+
+   await connectApi.createVideo(titulo, descricao, url, imagem);
+
+
+    window.location.href = "../pages/envio-concluido.html";
+}
+
+form.addEventListener("submit", evento => creatingVideo(evento))
+
